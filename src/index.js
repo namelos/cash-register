7f0d87df692e9ca24a76a856271c3dfc78080d62
namelos <namelos@outlook.com>
2016-03-11 21:02:24 +0800

    add model module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+import { Observable } from 'rx'
+import { run } from '@cycle/core'
+import { hJSX, makeDOMDriver } from '@cycle/dom'
 import input from './input.json'
 import config from './config.json'
+const { from, of } = Observable
+
+const main = () => {
+  return {
+    DOM: from([1, 2, 3]).map(x => <div>{ x }</div>)
+  }
+}
+
+run(main, {
+  DOM: makeDOMDriver('#app')
+})
--- src/model.js
+++ src/model.js
@@ -1,3 +1,13 @@
 import { Observable } from 'rx'
 import { contains, head, last, add, subtract, multiply, prop, __ } from 'ramda'
+import { parseString } from './lib'
 const { from, zip } = Observable
+
+// 主逻辑模块, 接受条形码输入以及商品信息, 返回用于渲染的数据结构
+export default model = (input, config) => {
+  // 将输入的数组转化为流, 将条形码字符串分割, 根据数组序对头部(商品类别category)分类支流
+  const items$$ = from(input).map(parseString).groupBy(head)
+
+  //
+  const category$ = items$$.flatMap(item$ => item$.map(head).last())
+}
